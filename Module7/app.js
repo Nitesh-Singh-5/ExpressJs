f7ab752a3750144fd26767a4ca31e343106e9997
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2022-01-19 22:10:44 +0530

    Added Static Files

--- Module7/app.js
+++ Module7/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import student from './routes/student.js';
+import { join } from 'path';
+
 const app = express()
 const port = process.env.PORT || '3000'
-import student from './routes/student.js';
 
+// Static Files
+app.use(express.static(join(process.cwd(), '/public')));
 
+// Load Routes
 app.use('/student', student);
 
 app.listen(port, () => {
